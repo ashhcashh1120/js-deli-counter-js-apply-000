d103bb0ccf0904a2157bb67394fdc0df04441e55
Ashley Achibald <aarchibald20@gmail.com>
2018-12-20 16:20:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(katzDeliLine, name){
   var num = katzDeliLine.length;
   return `Welcome, ${name}. You are number ${num} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length >= 1){
+    var sentence = `Currently serving ${katzDeliLine[0]}.`
+    return sentence; 
+  } else {
+    return 'There is nobody waiting to be served!';
+  }
+}
